3d8fc9ffc05496572427bed9e93fffe5d832f541
Joe HurowitzE <joehurowitz@gmail.com>
2017-03-07 08:56:49 -0500

    Homework week 4

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+
+	
+			
+		$(document).ready(function() {
+
+			//var trackLosses = 0;
+			var wins = 0;
+			var losses = 0;
+			var random = Math.floor((Math.random()*120)+19);
+			var total=0;
+			var pinkRandom = Math.floor((Math.random() * 12) + 1);
+			var blueRandom = Math.floor((Math.random() * 12) + 1);
+			var yellowRandom = Math.floor((Math.random() * 12) + 1);
+			var greenRandom = Math.floor((Math.random() * 12) + 1);
+			
+
+			
+			$("#display-random").html(random);
+			init();
+
+			function init(){
+
+			$(".pink-box").on("click", function (){
+				total = total + pinkRandom;
+				$("#display-score").html(total);
+
+					if(total > random){ 
+				 losses = losses + 1; 
+				$("#wins-losses").html("Losses : " + losses);
+				
+				}
+				else if(total === random){
+					 wins = wins + 1;
+					$("#wins-losses").html("Wins : " + wins);
+				
+				}
+
+			});
+			
+
+			$(".blue-diamond").on("click", function (){
+				 total = total + blueRandom;
+				 $("#display-score").html(total);
+
+				 if(total > random){
+				 losses = losses + 1; 
+				$("#wins-losses").html("Losses : " + losses);
+				
+				}
+
+				else if(total === random){
+					 wins = wins + 1;
+					$("#wins-losses").html("Wins : " + wins);
+				
+				}
+
+			});
+
+			$(".yellow-octogon").on("click", function (){
+				total = total + yellowRandom;
+				$("#display-score").html(total);
+
+				if(total > random){
+				 losses = losses + 1; 
+				$("#wins-losses").html("Losses : " + losses);
+				
+				}
+				else if(total === random){
+					 wins = wins + 1;
+					$("#wins-losses").html("Wins : " + wins);
+				
+				}
+
+			});
+
+			$(".green-rectangle").on("click", function (){
+				 total = total + greenRandom;
+				 $("#display-score").html(total);
+			
+				 if(total > random){
+				 losses = losses + 1; 
+				$("#wins-losses").html("Losses : " + losses);
+				
+				}
+				else if(total === random){
+					 wins = wins + 1;
+					$("#wins-losses").html("Wins : " + wins);
+				
+				}
+
+			});
+
+			}
+
+
+
+		});
